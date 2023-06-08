5246d60cb54a258598044a5c566d7391c98152f9
Jay Skelly <jason.skell@gmail.com>
2023-06-08 12:41:07 +0200

    map markers

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,8 @@ class Space < ApplicationRecord
   has_many_attached :photos
   has_many :bookings
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, :address, :price, :description, :availability, presence: true
 end
