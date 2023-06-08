217ae5509d736e9ecc6a731dcaf3eff460ef59ba
Tristan Meillard <meillard.tristan@gmail.com>
2023-06-08 15:23:34 +0200

    search bar

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,5 @@
 class Space < ApplicationRecord
+  # searchkick
   has_many_attached :photos
   has_many :bookings
   belongs_to :user
@@ -6,4 +7,11 @@ class Space < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, :address, :price, :description, :availability, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+  against: [ :name, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
